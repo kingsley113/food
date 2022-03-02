c393618137e3f54a2a8f5442faa29c625e2ea4f2
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-03-02 01:41:48 -0800

    create results card

---
+++ src/components/ResultCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+
+const ResultCard = () => {
+  return (
+    <View>
+      <Text>Result Card</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultCard;
