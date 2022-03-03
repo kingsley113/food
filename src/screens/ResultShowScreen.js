b8083c0c6336665d88079a14c59435cb8d42e951
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-03-03 01:04:22 -0800

    Create results show screen

---
+++ src/screens/ResultShowScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
+
+const ResultShowScreen = () => {
+  return (
+    <View>
+      <Text>Results Show Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultShowScreen;
